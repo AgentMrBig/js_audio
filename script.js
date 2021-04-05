d6c5f2e1090061e4c647ca96e0e538239806d94b
AgentMrBig <agentmrbig@gmail.com>
2021-04-05 02:21:04 -0400

    play a music file and display an animated bar animation for the sound being played

--- script.js
+++ script.js
@@ -1,14 +1,42 @@
-const button1 = document.getElementById("button1");
-let audio1 = new Audio();
-audio1.src =
-  "./audio/Epic Dark Mysterious Music (Copyright and Royalty Free) Black Fingerprint.mp3";
+const container = document.getElementById("container");
+const canvas = document.getElementById("canvas1");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+const ctx = canvas.getContext("2d");
+let audioSource;
+let analyser;
 
-button1.addEventListener("click", function () {
+container.addEventListener("click", function () {
+  //let audio1 = new Audio();
+  const audio1 = document.getElementById("audio1");
+  audio1.src =
+    "./audio/Epic Dark Mysterious Music (Copyright and Royalty Free) Black Fingerprint.mp3";
+  const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+
   audio1.play();
-  audio1.addEventListener("playing", function () {
-    console.log("Audio 1 started playing!");
-  });
-  audio1.addEventListener("ended", function () {
-    console.log("Audio 1 ended!");
-  });
+  audioSource = audioContext.createMediaElementSource(audio1);
+  analyser = audioContext.createAnalyser();
+  audioSource.connect(analyser);
+  analyser.connect(audioContext.destination);
+  analyser.fftSize = 64;
+  const bufferLength = analyser.frequencyBinCount;
+  const dataArray = new Uint8Array(bufferLength);
+
+  const barWidth = canvas.width / bufferLength;
+  let barHeight;
+  let x;
+
+  function animate() {
+    x = 0;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    analyser.getByteFrequencyData(dataArray);
+    for (let i = 0; i < bufferLength; i++) {
+      barHeight = dataArray[i];
+      ctx.fillStyle = "white";
+      ctx.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
+      x += barWidth;
+    }
+    requestAnimationFrame(animate);
+  }
+  animate();
 });
